98c1307ea5bee3f5749dcd003d1787181703ae72
Garth Braithwaite <garthdb@gmail.com>
2012-11-15 14:46:50 -0800

    adjusted colors and added animation for transitions

--- element-highlight/js/app.coffee
+++ element-highlight/js/app.coffee
@@ -1,6 +1,8 @@
+
 $ ->
   refresh()
   $('#frame').load ()->
+    @delay = (ms, func) -> setTimeout func, ms
     htmlstring = '<ul>'
     @elements = $("#frame").contents().find('body *')
     for element in @elements
@@ -10,14 +12,24 @@ $ ->
     $('body > aside a').click (e)=>
       index = $(e.target).parent().index()
       @selectedElement = @elements[index]
-      offset = $(@selectedElement).offset()
-      overlayStyles = "outline: 4px solid rgba(94, 167, 255, 1); box-shadow: 0 0 30px #42adfc; width:#{$(@selectedElement).width() - 2}px; height:#{$(@selectedElement).height() - 2}px; position:absolute; top:#{offset.top}px; left:#{offset.left + 300}px;"
-      overlay = "<div class='brackets-highlight' data-highlight-index='#{index}' style='#{overlayStyles}'></div>"
-      
-      if ($('#content .brackets-highlight[data-highlight-index="'+index+'"]').length > 0)
-        $('#content .brackets-highlight[data-highlight-index="'+index+'"]').remove()
+      existingElement = $('#content .brackets-highlight[data-highlight-index="'+index+'"]')
+      if (existingElement.length > 0)
+        $(existingElement).css('opacity', '0')
+        @delay 200, ->
+          existingElement.remove()
       else
+        offset = $(@selectedElement).offset()
+        overlayStyles = "background: rgba(94,167,255, 0.1);"
+        overlayStyles += "box-shadow: 0 0 8px 2px rgba(94,167,255, 0.3), inset 0 0 4px 1px rgba(255,255,255,0.6);"
+        overlayStyles += "opacity: 0; -webkit-transition: opacity 0.2s; width:#{$(@selectedElement).width() - 2}px; height:#{$(@selectedElement).height() - 2}px; position:absolute; top:#{offset.top}px; left:#{offset.left + 300}px;"
+        overlay = document.createElement('div')
+        $(overlay).addClass('brackets-highlight')
+        $(overlay).attr('data-highlight-index', index)
+        $(overlay).attr('style', overlayStyles)
         $("#content").append overlay
+        @delay 1, ->
+          $(overlay).css('opacity', '1')
+        
       return false
 
 refresh = (url) ->
--- element-highlight/js/app.js
+++ element-highlight/js/app.js
@@ -6,6 +6,9 @@
     return $('#frame').load(function() {
       var element, htmlstring, _i, _len, _ref,
         _this = this;
+      this.delay = function(ms, func) {
+        return setTimeout(func, ms);
+      };
       htmlstring = '<ul>';
       this.elements = $("#frame").contents().find('body *');
       _ref = this.elements;
@@ -16,16 +19,28 @@
       htmlstring += '</ul>';
       $($('body > aside')[0]).html(htmlstring);
       return $('body > aside a').click(function(e) {
-        var index, offset, overlay, overlayStyles;
+        var existingElement, index, offset, overlay, overlayStyles;
         index = $(e.target).parent().index();
         _this.selectedElement = _this.elements[index];
-        offset = $(_this.selectedElement).offset();
-        overlayStyles = "outline: 4px solid rgba(94, 167, 255, 1); box-shadow: 0 0 30px #42adfc; width:" + ($(_this.selectedElement).width() - 2) + "px; height:" + ($(_this.selectedElement).height() - 2) + "px; position:absolute; top:" + offset.top + "px; left:" + (offset.left + 300) + "px;";
-        overlay = "<div class='brackets-highlight' data-highlight-index='" + index + "' style='" + overlayStyles + "'></div>";
-        if ($('#content .brackets-highlight[data-highlight-index="' + index + '"]').length > 0) {
-          $('#content .brackets-highlight[data-highlight-index="' + index + '"]').remove();
+        existingElement = $('#content .brackets-highlight[data-highlight-index="' + index + '"]');
+        if (existingElement.length > 0) {
+          $(existingElement).css('opacity', '0');
+          _this.delay(200, function() {
+            return existingElement.remove();
+          });
         } else {
+          offset = $(_this.selectedElement).offset();
+          overlayStyles = "background: rgba(94,167,255, 0.1);";
+          overlayStyles += "box-shadow: 0 0 8px 2px rgba(94,167,255, 0.3), inset 0 0 4px 1px rgba(255,255,255,0.6);";
+          overlayStyles += "opacity: 0; -webkit-transition: opacity 0.2s; width:" + ($(_this.selectedElement).width() - 2) + "px; height:" + ($(_this.selectedElement).height() - 2) + "px; position:absolute; top:" + offset.top + "px; left:" + (offset.left + 300) + "px;";
+          overlay = document.createElement('div');
+          $(overlay).addClass('brackets-highlight');
+          $(overlay).attr('data-highlight-index', index);
+          $(overlay).attr('style', overlayStyles);
           $("#content").append(overlay);
+          _this.delay(1, function() {
+            return $(overlay).css('opacity', '1');
+          });
         }
         return false;
       });
